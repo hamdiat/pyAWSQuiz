aa732800ffc2f8458ba2f68a2bf3a30104d68493
hamdiat <61632232+hamdiat@users.noreply.github.com>
2022-10-04 15:08:54 -0400

    Python AWS Quiz

---
+++ quiz_game.py
@@ -0,0 +1,84 @@
+print("Welcome to our AWS Acronym Quiz!")
+
+playing = input("Are you ready to play? ")
+
+if playing.lower() != "yes":
+    print("Cool, come back when you want to play")
+    quit()
+
+print("Okay! Let's play :)")
+score = 0
+
+answer = input("What is the full meaning of Amazon SNS? ")
+if answer.lower() == "simple notification system":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does EC2 stand for? ")
+if answer.lower() == "elastic compute cloud":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does IAM stand for? ")
+if answer.lower() == "identity and access management":
+    print("Correct! You are on roll!")
+    score += 1
+else:
+    print("Incorrect! Don't worry you can still get the next one")
+
+answer = input("What is the full meaning of ECR? ")
+if answer.lower() == "elastic container registry":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does ECS stand for? ")
+if answer.lower() == "ec2 container service":
+    print("Correct! whoo! let's do this!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does AMI stand for? ")
+if answer.lower() == "amazon machine image":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does AZ stand for? ")
+if answer.lower() == "availability zone":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does DMS stand for? ")
+if answer.lower() == "database migration service":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does EKS stand for? ")
+if answer.lower() == "elastic kubernetes service":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does EBS stand for? ")
+if answer.lower() == "elastic block store":
+    print("Correct! You are simply a genius!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
+print("That is " + str((score / 10) * 100) + "%.")
+print("We hope that was fun! Do come back again! :)")
